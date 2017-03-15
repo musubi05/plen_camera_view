48fc6cf3f4f867ac9f1296b437ccbd5fd50436f6
Yugo Kajiwara <y.kaji.oita@gmail.com>
2017-03-15 16:58:23 +0900

    Add viewer program

--- scripts/viewer.py
+++ scripts/viewer.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-__author__    = 'Tatsuroh SAKAGUCHI'
 __author__    = 'Yugo KAJIWARA'
+__author__    = 'Tatsuroh SAKAGUCHI'
 __copyright__ = 'PLEN Project Company, and all authors.'
-__license__   = 'BSD'
+__license__   = 'MIT'
 
 import subprocess
-
+import os
 import rospy
 from sensor_msgs.msg import CompressedImage
 from std_msgs.msg import Empty
@@ -24,14 +24,21 @@ class Node(object):
         # register publisher wanted to publish message of web camera
         self.stream_topic = rospy.Publisher('request_capture', Empty, queue_size=10)
         self.subscriber = rospy.Subscriber('camera_stream', CompressedImage, self.subscribe)
+        self.streaming = rospy.Publisher('camera_streaming', CompressedImage, queue_size=10)
 
         self.sleep_rate = rospy.Rate(self.SLEEP_RATE_HZ)
+
+	if not os.path.exists('/tmp/ros/camera'):
+            os.makedirs('/tmp/ros/camera')
+
+	subprocess.Popen(['feh', '-Z', '-R', '0.075', '/tmp/ros/camera'])
 
     def subscribe(self, message):
         rospy.loginfo('RECEIVED IMAGE')
         f = open('/tmp/ros/camera/received_image.jpg', 'w')
         f.write(message.data)
         f.close()
+        self.streaming.publish(message)
 
     def start(self):
         try:
